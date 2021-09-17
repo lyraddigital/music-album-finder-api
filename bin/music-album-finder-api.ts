0d1b8260040e7f214168456e13fd1cdf25d4f5a9
darylduck <daryl.duckmanton@gmail.com>
2021-09-17 22:23:46 +1000

    Updated a specific stack name

--- bin/music-album-finder-api.ts
+++ bin/music-album-finder-api.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+
 import { MusicAlbumFinderApiStack } from '../lib/music-album-finder-api-stack';
 
 const app = new cdk.App();
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
-new MusicAlbumFinderApiStack(app, 'SpotifyCloneWebsiteStack', { env });
+new MusicAlbumFinderApiStack(app, 'SpotifyCloneWebsiteStack_Production', { env });
