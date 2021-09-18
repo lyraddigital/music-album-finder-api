8ffd450e4da9ce52d1cddf4cee34da938fc41c50
darylduck <daryl.duckmanton@gmail.com>
2021-09-18 12:23:57 +1000

    Updated the stack name to an acceptable and removed the ls command from the install dependencies step

--- bin/music-album-finder-api.ts
+++ bin/music-album-finder-api.ts
@@ -7,4 +7,4 @@ import { MusicAlbumFinderApiStack } from '../lib/music-album-finder-api-stack';
 const app = new cdk.App();
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
-new MusicAlbumFinderApiStack(app, 'SpotifyCloneWebsiteStack_Production', { env });
+new MusicAlbumFinderApiStack(app, 'SpotifyCloneWebsiteStack-Production', { env });
